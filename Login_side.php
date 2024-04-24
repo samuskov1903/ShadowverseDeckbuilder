ee34dbcec27066888abb0b2778226cee05fac78c
samuskov1903 <71513518+samuskov1903@users.noreply.github.com>
2024-04-24 13:21:08 +0200

    Add files via upload

---
+++ Login_side.php
@@ -0,0 +1,62 @@
+<?php
+$servername = "localhost";
+$username = "samuskov";
+$password = ""; // no password
+$dbname = "shadowversedata";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+session_start();
+
+if(isset($_POST['username']) && isset($_POST['password'])) {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    // SQL query to check if email or username already exists
+    $sql = "SELECT * FROM brugere WHERE Password = '$password' AND Username = '$username'";
+    $result = $conn->query($sql);
+
+    if ($result->num_rows == 0) {
+        // output message if user doesn't exist
+        echo "User doesn't exist";
+    } else {
+        $sql = "SELECT Bruger_ID FROM brugere WHERE Password = '$password' AND Username = '$username'";
+        $result = $conn->query($sql);
+        if ($result->num_rows > 0) {
+            $row = $result->fetch_assoc();
+            $bruger_id = $row['Bruger_ID'];
+            echo "User logged in successfully";
+            $_SESSION['bruger_id'] = $bruger_id;
+            echo $bruger_id;
+            header("Location: Mypage.php");
+            exit;
+        }
+    }
+}
+?>
+
+
+<!DOCTYPE html>
+<html lang="da">
+
+<head>
+    <title>Loginpage</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+
+<body>
+    <h1>Login</h1>
+    <form method="POST">
+        <input type="text" name="username" placeholder="Username"> <br>
+        <input type="password" name="password" placeholder="Password"> <br>
+        <input type="submit" value="Login" >
+        <p>Don't have an account? <a href="Opret_bruger.php">Create one here</a></p>
+
+
+    </form>
+</body>
---
+++ Opret deck.php
@@ -0,0 +1,95 @@
+<?php
+$servername = "localhost";
+$username = "samuskov";
+$password = ""; // no password
+$dbname = "shadowversedata";
+
+//test variables
+$bruger_id = 1;
+ $_SESSION['bruger_id'] = $bruger_id;
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+ 
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+session_start();
+if (isset($_POST['deckname'])) {
+    $_SESSION['deckname'] = $_POST['deckname']; // Store the variable in the session
+}
+if (isset($_POST['Class'])) {
+    $_SESSION['Class'] = $_POST['Class']; // Store the variable in the session
+}
+
+
+
+if(isset($_POST['deckname'])){
+    if($_POST['Class'] == "None"){
+        echo "Please choose a class for your deck";
+    }
+    else{
+        $deckname = $_POST['deckname'];
+        $Class = $conn->real_escape_string($_POST['Class']);
+        $sql = "SELECT Klasse_ID FROM klasse WHERE Navn = '$Class'";
+        $result = $conn->query($sql);
+
+        if ($result->num_rows > 0) {
+            // output data of each row
+            while($row = $result->fetch_assoc()) {
+                $klasse_id = $row["Klasse_ID"];
+            }
+        } else {
+            echo "No results";
+            exit;
+        }
+
+        // Check if Klasse_ID equals 0
+        if ($klasse_id == 0 and $deckname) {
+            echo "Error: Klasse_ID cannot be 0";
+            exit;
+        }
+        $sql = "INSERT INTO decks (Navn, Klasse_ID, Bruger_ID) VALUES ('$deckname', '$klasse_id','$bruger_id')";
+        if ($conn->query($sql) === TRUE) {
+            echo "Deck created successfully";
+        } else {
+            echo "Error: " . $sql . "<br>" . $conn->error;
+        }
+        header('Location: Deckbuilder.php');
+        exit;
+    }
+}
+?>
+<!DOCTYPE html>
+<html lang="da">
+
+<head>
+    <title>Deck builder</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+
+<body>
+    <h1>Opret deck</h1>
+    <form method="POST">
+        <input type="text" name="deckname" placeholder="Deckname"> <br>
+        Please choose a Class for your deck: <br>
+        <select name="Class">
+            <option value="None">None</option>
+            <option value="Neutral">Neutral</option>
+            <option value="Forestcraft">Forestcraft</option>
+            <option value="Swordcraft">Swordcraft</option>
+            <option value="Runecraft">Runecraft</option>
+            <option value="Dragoncraft">Dragoncraft</option>
+            <option value="Shadowcraft">Shadowcraft</option>
+            <option value="Bloodcraft">Bloodcraft</option>
+            <option value="Havencraft">Havencraft</option>
+            <option value="Portalcraft">Portalcraft</option>
+        </select> <br>
+        <input type="submit" value="Create deck" >
+
+        
+    </form>
+</body>
+
+</html>
