f7df007254728d47fb6ae941163dde6c5ae0eb77
Samuskov1903 <samuel@kjaerskov.dk>
2024-04-26 13:01:22 +0200

    Adding

--- Mypage.php
+++ Mypage.php
@@ -12,10 +12,15 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 session_start();
-if (isset($_POST['Bruger_ID'])) {
-    $_SESSION['Bruger_ID'] = $_POST['Bruger_ID']; // Store the variable in the session
+$bruger_id = $_SESSION['bruger_id'];
+$sql = "SELECT Username FROM brugere WHERE Bruger_ID = '$bruger_id'";
+$result = $conn->query($sql);
+if ($result->num_rows > 0) {
+    // output data of each row
+    while ($row = $result->fetch_assoc()) {
+        $bruger_navn = $row["Username"];
+    }
 }
-
 ?>
 <!DOCTYPE html>
 <html lang="da">
@@ -23,7 +28,24 @@ if (isset($_POST['Bruger_ID'])) {
 <head>
     <title>Opret_bruger</title>
     <link rel="stylesheet" type="text/css" href="style.css">
+    <h1>My page</h1>
+    <br>
+    <h2> <?php
+    echo $bruger_navn;
+    ?> </h2>
+    <br>
+<h2> Your decks</h2>
     <?php
-    $sql = "SELECT Bruger_ID FROM brugere where Bruger_ID = '$Bruger_ID'";
-jjjjjjjjj    ?>
+    $sql = "SELECT * FROM decks WHERE Bruger_ID = '$bruger_id'";
+    $result = $conn->query($sql);
+    if ($result->num_rows > 0) {
+            // output data of each row
+            while($row = $result->fetch_assoc()) {
+                echo "<a href='Deckbuilder.php?deck_id=".$row["Deck_ID"]."&deckname=".$row["Navn"]."'>Name: " . $row["Navn"]. "</a><br>";
+                if (isset($_GET['deckname'])) {
+                    $_SESSION['deckname'] = $_GET['deckname'];
+}
+            }
+    }
+?>klj
 </head>
